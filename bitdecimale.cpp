34059e16cd76f2c8acc1d29a811f4ce8f229122b
Conti-Matias <113691220+Conti-Matias@users.noreply.github.com>
2022-12-09 19:04:43 +0100

    Update bitdecimale.cpp

--- bitdecimale.cpp
+++ bitdecimale.cpp
@@ -1,8 +1,8 @@
 /** ****************************************************************************************
 * \mainpage fattorialenumero
 * Scrivere un programma che converte un numero binario a 8 bit in un numero decimale. 
-* L’utente inserisce le cifre del numero binario un bit alla volta, partendo dal bit più significativo. 
-* Il programma dovrà visualizzare il numero decimale corrispondente. Suggerimento: per calcolare le potenze
+* Lâ€™utente inserisce le cifre del numero binario un bit alla volta, partendo dal bit piÃ¹ significativo. 
+* Il programma dovrÃ  visualizzare il numero decimale corrispondente. Suggerimento: per calcolare le potenze
 * di 2 utilizzare la funzione pow includendo la libreria math.h.
 * @author Conti Gallenti Matias
 * @date 08/12/2022
@@ -14,16 +14,16 @@
 int main()  
 {
     printf("Matias Conti Gallenti\n");
-    int n1=0, ris=7,BIT;
+    int n1=0, cont=7, int BIT;
 
     printf("inserire 8 bit\n");    
-    do
+    do		//fare
 		{
-        	scanf("%d", &BIT);          
-			n1 += pow(2, ris) * BIT;   
-			ris=ris-1;                       
+        	scanf("%d", &BIT);     //non ripete ogni volta la richiesta    
+			n1 += pow(2, ris) * BIT;   //convero il bit in decimale
+			cont=cont-1;                 //tolgo a cont 1       
     	} 	
-	while (ris>-1);               
+	while (cont>-1);  //mentre cont Ã¨ maggiore di -1             
 
-    printf("binario=%d", n1);         
+    printf("binario=%d", n1);        
 }
