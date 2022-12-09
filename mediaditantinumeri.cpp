459ed6ad8460a9596a9c0ad4f509889d937cb583
Conti-Matias <113691220+Conti-Matias@users.noreply.github.com>
2022-12-09 18:13:47 +0100

    Update mediaditantinumeri.cpp

--- mediaditantinumeri.cpp
+++ mediaditantinumeri.cpp
@@ -1,7 +1,7 @@
 /** ****************************************************************************************
 * \mainpage mediaditantinumeri
 * Si scriva un programma per calcolare la media aritmetica di una serie di numeri inseriti da tastiera.
-* L�introduzione del valore 0 indica il termine del caricamento dei dati.
+* L’introduzione del valore 0 indica il termine del caricamento dei dati.
 * @author Conti Gallenti Matias
 * @date 30/11/2022
 */
@@ -10,21 +10,22 @@
 
 int main() 
 {
-	int n1,n2=0;
-	float media=0;
-	printf("calcolo della media dei numeri inseriti, scrivere 0 per terminare\n");
-	do
+	int n1;
+	int n2=0;		//contatore
+	float media=0;		//risultato
+	printf("Conti Gallenti Matias\ncalcolo della media dei numeri inseriti, scrivere 0 per terminare\n");
+	do				//fare
 	{
 		printf("Inserisci un numero: ");
-	    scanf("%d",&n1);
-		if(n1!=0)
+	    	scanf("%d",&n1);
+		if(n1!=0)		//se n1 è diverso da 0 fare
 		{
-			media=media+n1;
-			n2=n2+1;
+			media=media+n1; //prima parte del calcolo della media
+			n2=n2+1;	
 		}
 	}
-	while(n1!=0);
-	media=media/n2;
+	while(n1!=0);			//finche n1 è diverso da 0		
+	media=media/n2;			//calcolo media
 	printf("la media e' %f",media);
 	return 0;
 }
