32a96eac13f2b95bdc0b315eaf0c38a8ba6fd91b
Conti-Matias <113691220+Conti-Matias@users.noreply.github.com>
2022-12-09 18:06:30 +0100

    Update coppiedinumeri.cpp

--- coppiedinumeri.cpp
+++ coppiedinumeri.cpp
@@ -1,7 +1,7 @@
 /** ****************************************************************************************
 * \mainpage coppiedinumeri
 * Scrivere un programma che visualizzi tutte le 
-* coppie ordinate di numeri naturali la cui somma è 15.
+* coppie ordinate di numeri naturali la cui somma Ã¨ 15.
 * @author Conti Gallenti Matias
 * @date 30/11/2022
 */
@@ -10,16 +10,17 @@
 
 int main() 
 {
-	int n1=15,n2=0,n3=0;
+	int n1=15,n2=0;
+	int n3=0;		//contatore
 	
-	do
+	do			//ciclo per visualizzare tutte le possibile coppie di somme con risultato 15
 	{
-		n1=n1-1;
+		n1=n1-1;	
 		n2=n2+1;
 		n3=n3+1;
 		printf("%d+%d\t",n2,n1);
 	}
-	while(n3<7);
+	while(n3<7);		
 	return 0;
 }
 
