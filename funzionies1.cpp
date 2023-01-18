60acc19fcb2b87227ef07d6b7e66998958721fc9
Conti-Matias <113691220+Conti-Matias@users.noreply.github.com>
2023-01-18 13:45:44 +0100

    Add files via upload

--- funzionies1.cpp
+++ funzionies1.cpp
@@ -1,23 +1,77 @@
 /*******************************************************************************************
-* \mainpage funzione somma                                                                 *
+* \mainpage 																			   *
+*1) Scrivere una funzione chiamata "mcd" che prende in input due numeri interi e           *
+*  restituisce il loro massimo comune divisore utilizzando l'algoritmo di Euclide.         *
+2)Scrivere una funzione chiamata "somma_divisori" che prende in input un numero            *
+*  intero e restituisce la somma dei suoi divisori.                                        *
+3)Scrivere una funzione chiamata "area_rettangolo" che prende in input la base             *
+*  e l'altezza di un rettangolo e restituisce l'area. 									   *
 * @author Conti Gallenti Matias                                                            *
-* @date 11/01/2023                                                                         *                                                                                        *
+* @date 11/01/2023                                                                         *                                                                                        
 ********************************************************************************************/
-#include <stdio.h>      
-#define N 5
-#define n 3
-int somma (int , int);
-int main()  
+#include <stdio.h>   
+#include <stdlib.h>    
+#define N 15
+#define C 5
+
+int MCD(int, int);
+int somma_divisore(int);
+int area_rettangolo(int, int);
+
+int main()
 {
-   int r=0;
-   r=somma (n,N);
-   printf("%d",r);
+	//MCD
+	int f=0;
+	int r=0;
+	int n1;
+	int ris=0;
+	int base, altezza;
+	f=MCD(N,C);
+	printf("l'MCD tra 15 e 5 e' %d\n",f);
+	//somma_divisore
+	printf("Inserisci il primo numero: ");
+	scanf("%d",&n1);
+	r=somma_divisore(n1);
+	printf("la somma dei divisori e' %d\n",r);
+	//area_rettangolo
+	printf("Inserisci la base: ");
+	scanf("%d",&base);
+	printf("Inserisci l'altezza: ");
+	scanf("%d",&altezza);
+	ris=area_rettangolo(base, altezza);
+	printf("%d",ris);
 }
 
-int somma(int x, int y)
+int MCD(int x, int y)//funzione per l'MCD
 {
-	int s;
-	s=x+y;
-	return s;
+	int ris=0;
+	while(y!=0)
+	{
+		ris=x%y;
+		x=y;
+		y=ris;
+	}
+	return x;
+}
+
+int somma_divisore(int x)//funzione per la somma dei divisori
+{
+	int ris=0;
+	int i=0;
+	for(i=1;i<=x;i++)
+	{
+		if(x%i==0)
+		{
+			ris=ris+i;	
+		}	
+	}
+	return ris;
+}
+
+int area_rettangolo(int x, int y)//funzione per trovare 
+{
+	int r=0;
+	r=x*y;
+	return r;
 }
 
