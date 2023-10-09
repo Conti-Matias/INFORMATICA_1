8b8cbb42ed7d830f40cfec798309794a32bfc014
Conti-Matias <113691220+Conti-Matias@users.noreply.github.com>
2023-10-09 09:51:40 +0200

    Add files via upload

--- rec.colonna.cpp
+++ rec.colonna.cpp
@@ -17,12 +17,12 @@
 
 #define N 20
 #define V 10
-#define C 5
+#define C 1
 
 struct data 
 {
     int giorno;
-    char mese[12];
+    int mese;
     int anno;
 } typedef data;
 
@@ -36,14 +36,32 @@ struct s_alunno
 } typedef s_alunno;
 
 void caricaTab(s_alunno[]);
-void stampaTab(s_alunno[]);
+void stampa(s_alunno[]);
+int ricercaCog(s_alunno[], char[]);
+int stampaMedia(s_alunno[]);
+int studenti10(s_alunno[]);
 
 int main()
 {
 	s_alunno s1[C];
 	srand(time(NULL));
+	int r=0;
+	char l[N];
     caricaTab(s1);
-    stampaTab(s1);
+    stampa(s1);
+
+    printf("\n\ncognome da cercare: ");
+    scanf("%s", l);
+
+    r = ricercaCog(s1, l);
+    if(r!=-1) printf("\n\ncognome trovato: %s\n\n", s1[r].cognome, r);
+    else printf("cognome non trovato\n\n");
+
+    r = stampaMedia(s1);
+    printf("\n %d hanno la media dei voti >= 6\n", r);
+
+    r = studenti10(s1);
+    printf("\nstudenti con almeno un 10: %d\n\n", r);
 }
 
 void caricaTab(s_alunno s1[])
@@ -58,15 +76,9 @@ void caricaTab(s_alunno s1[])
 		printf("inserisci il nome:");
 		scanf("%s",s1[i].nome);
 	
-		printf("inserisci il giorno di nascita:");
-		scanf("%s",&s1[i].nascita.giorno);
-	
-		printf("inserisci il mese di nascita:");
-		scanf("%s",s1[i].nascita.mese);
-		fflush(stdin);
-	
-		printf("inserisci l'anno' di nascita:");
-		scanf("%s",&s1[i].nascita.anno);
+		s1[i].nascita.giorno = rand()%30+1;
+        s1[i].nascita.mese = rand()%12+1;
+        s1[i].nascita.anno = rand()%30+1990;
 	
 		printf("\n");
 		for(int j=0; j<V; j++)
@@ -74,10 +86,56 @@ void caricaTab(s_alunno s1[])
 	}
 }
 
-void stampaTab(s_alunno s1[])
+void stampa(s_alunno s1[])
 {
-	printf("matricola\t\tcognome\t\tnome\t\tgiorno\t\tmese\t\tanno\t\tvoto");
-	
+    printf("matricola\tcognome\t\tnome\tgiorno\t\tmese\t\tanno\t\tvoti\n");
+	for(int i = 0; i<C; i++)
+    {
+        printf("%d%15s%15s%15d%15d%15d\t\t",s1[i].matricola,s1[i].cognome,s1[i].nome,s1[i].nascita.giorno,s1[i].nascita.mese,s1[i].nascita.anno);
+        
+        
+            printf("%d",s1[i].voti);
+        printf("\n");
+    }
+    
+    
+}
+
+int ricercaCog(s_alunno s1[], char cog[])
+{
+    for(int i = 0; i<C; i++)
+    {
+        if(strcmp(s1[i].cognome, cog) == 0) return i;
+    }
+    return -1;
+}
+
+int stampaMedia(s_alunno s1[])
+{
+    int c = 0;
+    for(int i = 0, m=0; i < C; i++, m=0)
+    {
+        for(int j = 0; j<V; j++)
+            m += s1[i].voti[j];
+        m /= V;
+        if(m>=6) c++;
+        printf("\nmedia studente %s: %d", s1[i].cognome, m);
+    }
+    return c;
+}
+
+int studenti10(s_alunno s1[])
+{
+    int c = 0;
+    for(int i = 0; i<C; i++)
+    {
+        for(int j = 0; j<V; i++)
+            if(s1[i].voti[j] == 10){
+                c++;
+                j = V;
+            }
+    }
+    return c;
 }
 
 
