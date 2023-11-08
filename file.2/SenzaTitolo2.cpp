327431b916dc71e65e9a0618ea1f6f46281f5f92
Conti-Matias <113691220+Conti-Matias@users.noreply.github.com>
2023-11-08 13:45:53 +0100

    Update SenzaTitolo2.cpp

--- file.2/SenzaTitolo2.cpp
+++ file.2/SenzaTitolo2.cpp
@@ -1,12 +1,4 @@
-/*  legge  carattere per carattere il contenuto del file in.txt e lo ricopia nel file out.txt. 
-* 2)Scrivi  un  programma  che  legge  il  file  nomi.txt e  scrive  un  file  NOMI2.TXT,  
-*  dove  tutti  i  nomi  sono stati trasformati in caratteri maiuscoli.
-* 3)Scrivi un programma che conta i caratteri, le parole e le righe presenti in un file di testo (non gli spazi, tabulazioni e a capo)
-* 4)Scrivi un programma che legge il file numeri.txt (cifre numeriche ascii)composto da una sequenza
-*  di numeri di una cifra separati da uno spazio bianco e successivamente scrive 2 file, pari.txt e dispari.txt,
-*  nei quali dovrai inserire i numeri che hai letto a seconda del loro valore pari o dispari
-*5)Scrivere le funzioni cesareCrypt e cesareDecrypt che criptano o decriptano un file in input di una chiave key,
-*  usando l’algoritmo di Cesare.                                                                                     
+/*                                                                                      
 *********************************************************************************************/
 
 //------------------------------------------------------------------------------------------
