6c5687547acce311f593dd3bf0f71e07a26383e7
Conti-Matias <113691220+Conti-Matias@users.noreply.github.com>
2024-10-17 09:06:25 +0200

    Create index

---
+++ PHP/ES02/es_A/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <title>login</title>
+</head>
+<body>
+    <h4>richiesta credenziali per l'accesso nell'area riservata<h4>
+        <form name="frmLogin" action="login.php" method="POST">
+            User name: <input type="text" name="username"><br>
+            password: <input type="password" name="password"><br>
+            <input type="submit" value="submit">
+</form>    
+</body>
+</html>
